3e8b7dbae2f1ba7d38c86e1d77653193bf1ad03e
Dharmil <dharmil132@gmail.com>
2025-08-14 13:24:23 +0530

    For profile

--- src/main/java/model/SessionManager.java
+++ src/main/java/model/SessionManager.java
@@ -3,4 +3,15 @@ package model;
 public class SessionManager {
     // Static variable to hold username accessible everywhere
     public static String username;
+
+//    public static User currentUser;
+//
+//    public static void setCurrentUser(User user) {
+//        currentUser = user;
+//    }
+//
+//    public static User getCurrentUser() {
+//        return currentUser;
+//    }
+
 }
