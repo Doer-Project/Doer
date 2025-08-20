39460a8dc9f7df42bfa7efb1053fa55179f90ee7
Dharmil <dharmil132@gmail.com>
2025-08-20 09:15:22 +0530

    Ongoing work in houshold DB

--- src/main/java/database/household/OngoingWorkDAOImpl.java
+++ src/main/java/database/household/OngoingWorkDAOImpl.java
@@ -20,20 +20,19 @@ public class OngoingWorkDAOImpl implements OngoingWorkDAO {
         /// temporary solution ⬇️, This is not final we have to change it.
 
         List<OngoingWork> ongoingWorks = new ArrayList<>();
-        String sql = "SELECT title, description, preferred_work_date, status FROM workrequests WHERE household_id = ?";
+        String sql = "SELECT title, description, preferred_work_date FROM workrequests WHERE household_id = ?";
 
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-//            stmt.setString(1, username);
+            stmt.setString(1, username);
             /// checking
-        stmt.setInt(1,2);
+//        stmt.setInt(1,2);
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
                 ongoingWorks.add(new OngoingWork(
                         rs.getString("title"),
                         rs.getString("description"),
-                        rs.getString("preferred_work_date"),
-                        rs.getString("status")
+                        rs.getString("preferred_work_date")
                 ));
             }
         } catch (SQLException e) {
